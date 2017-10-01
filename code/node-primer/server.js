189352af2fae7513ce52c73f7b84eb2c4945cc80
baurine <2008.hbl@gmail.com>
2017-10-01 09:37:20 +0800

    Add request handlers

--- code/node-primer/server.js
+++ code/node-primer/server.js
@@ -1,12 +1,12 @@
 var http = require('http')
 var url = require('url')
 
-function start(route) {
+function start(route, handle) {
   function onResp(req, res) {
     var pathname = url.parse(req.url).pathname
     console.log(`Request for ${pathname} received.`)
 
-    route(pathname)
+    route(handle, pathname)
 
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.write('Hello World')
