1b19dc9ac479690d2d69d0727c988a2433b13412
baurine <2008.hbl@gmail.com>
2017-10-03 15:10:15 +0800

    User express router

--- code/myblog/index.js
+++ code/myblog/index.js
@@ -1,11 +1,9 @@
 var express = require('express')
 var app = express()
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-app.get('/', function(req, res) {
-  res.send('hello express')
-})
-app.get('/users/:name', function(req, res) {
-  res.send('hello ' + req.params.name)
-})
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 app.listen(3000)
