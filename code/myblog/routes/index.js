e76478df13fc454e6bf66d3fc04414549be64b12
baurine <2008.hbl@gmail.com>
2017-10-03 19:45:08 +0800

    404 page

--- code/myblog/routes/index.js
+++ code/myblog/routes/index.js
@@ -6,4 +6,11 @@ module.exports = function(app) {
   app.use('/signin', require('./signin'));
   app.use('/signout', require('./signout'));
   app.use('/posts', require('./posts'));
+
+  // 404 page
+  app.use(function (req, res) {
+    if (!res.headersSent) {
+      res.render('404');
+    }
+  });
 };
