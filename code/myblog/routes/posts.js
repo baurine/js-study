b8dc36bfcd2a6880a8b56fb15be5decf5ba7aebe
baurine <2008.hbl@gmail.com>
2017-10-03 18:09:23 +0800

    Display comments for post

--- code/myblog/routes/posts.js
+++ code/myblog/routes/posts.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var PostModel = require('../models/posts');
+var CommentModel = require('../models/comments');
 var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /posts 所有用户或者特定用户的文章页
@@ -66,16 +67,19 @@ router.get('/:postId', function(req, res, next) {
 
   Promise.all([
     PostModel.getPostById(postId),// 获取文章信息
+    CommentModel.getComments(postId),// 获取该文章所有留言
     PostModel.incPv(postId)// pv 加 1
   ])
   .then(function (result) {
     var post = result[0];
+    var comments = result[1];
     if (!post) {
       throw new Error('该文章不存在');
     }
 
     res.render('post', {
-      post: post
+      post: post,
+      comments: comments
     });
   })
   .catch(next);
