24ff7ab503b1f51edee02984d9f64d933d873235
baurine <2008.hbl@gmail.com>
2018-05-19 07:38:37 +0800

    MISC - Config auto generate html by html-webpack-plguin

--- codes/hello-webpack4/webpack.config.js
+++ codes/hello-webpack4/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -6,11 +7,16 @@ module.exports = {
     login: './src/login.js'
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.[hash:4].js', // 给文件名加上 4 位的 hash 值
     path: path.resolve('dist')
   },
   module: {},
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true // 在打包好的 bundle.js 后加上 "?xxx" hash 串
+    })
+  ],
   devServer: {},
   mode: 'development'
 }
