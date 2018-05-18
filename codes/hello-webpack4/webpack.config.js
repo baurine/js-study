21c49f21bff40bf124c5c18918161075f7b9f508
baurine <2008.hbl@gmail.com>
2018-05-19 07:10:43 +0800

    MISC - Update scripts in package.json

--- codes/hello-webpack4/webpack.config.js
+++ codes/hello-webpack4/webpack.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 module.exports = {
-  entry: '',
-  output: {},
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist')
+  },
   module: {},
   plugins: [],
   devServer: {},
